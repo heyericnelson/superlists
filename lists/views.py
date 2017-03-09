eaacc4ecb5c2b7c292da88ace35faffa4f9bd277
heyericnelson <heyericnelson@gmail.com>
2017-03-08 18:23:06 -0800

    add url for new lists to be added

--- lists/views.py
+++ lists/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import redirect,render
 from lists.models import Item
 
+# TODO: Allow multiple items to be displayed
+# TODO: Support multiple lists
+
 def home_page(request):
-    # TODO: Allow multiple items to be displayed
-    # TODO: Support multiple lists
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
